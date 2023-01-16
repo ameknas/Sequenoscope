c8261d3df73474d84a1dc1730028eb33dda4cef5
Abdallah Meknas <abdallah_meknas@gmail.com>
2023-01-16 14:05:52 -0500

    quick_edits

--- Sequenoscope/analyze/kat.py
+++ Sequenoscope/analyze/kat.py
@@ -1,17 +1,16 @@
-import subprocess
-
+from analyze import run_command
 
 def kat_sect(input_fastq, ref_seq):
     kat_command = "kat sect -o output.tsv " + ref_seq + " " + input_fastq
-    subprocess.run(kat_command, shell=True)
+    run_command(kat_command)
 
 def kat_filter(input_fastq, ref_seq):
     #generate a hash table based on ref fasta file
     hash_build = "kat filter kmer -o kat_generated_hash.kmer" + ref_seq
-    subprocess.run(hash_build, shell=True)
+    run_command(hash_build)
     #analyze based on generated hash
     filter_from_hash = "kat filter seq -o kat_filtered.fastq --seq " + input_fastq + " kat_generated_hash.kmer"
-    subprocess.run(filter_from_hash, shell=True)
+    run_command(filter_from_hash)
 
 
 
