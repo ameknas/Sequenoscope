21fdf5fc3ff1e42719540eba2f84312269b75526
Abdallah Meknas <abdallah_meknas@gmail.com>
2023-01-19 09:22:16 -0500

    class update for class

--- Sequenoscope/analyze/__init__.py
+++ Sequenoscope/analyze/__init__.py
@@ -1,4 +1,5 @@
 from subprocess import Popen, PIPE
+import os
 
 def run_command(command):
     p = Popen(command, shell=True, stdout=PIPE, stderr=PIPE)
@@ -6,6 +7,9 @@ def run_command(command):
     stdout = stdout.decode('utf-8')
     stderr = stderr.decode('utf-8')
     return stdout, stderr
+
+def is_non_zero_file(fpath):  
+    return os.path.isfile(fpath) and os.path.getsize(fpath) > 0
 
 class Sequence:
     technology = None
--- Sequenoscope/analyze/kat.py
+++ Sequenoscope/analyze/kat.py
@@ -17,38 +17,44 @@ def kat_filter(input_fastq, ref_seq):
 ref = "ref.fasta"
 
 class kat_analysis:
-    def __init__(self, input_seq, ref_seq):
-        self.input_seq = Sequence(input_seq.technology, input_seq.files)
-        self.ref_seq = ref
+    def __init__(self, input_path, ref_path, out_path):
+        self.input_path = input_path
+        self.ref_path = ref_path
+        self.out_path = out_path
 
     def kat_sect(self):
-        input_fastq = self.input_seq.out_files
-        ref_fasta = self.ref_seq
-        kat_sect_cmd = "kat sect -o output.tsv" + ref_fasta + " " + input_fastq
-        run_command(kat_sect_cmd)
-        self.check_for_errors("output.tsv")
+        input_fastq = self.input_path.out_files
+        ref_fasta = self.ref_path
+        out_file_sect = os.path.join(self.out_path, "output.tsv")
+        kat_sect_cmd = "kat sect -o {} {} {}".format(out_file_sect, ref_fasta, input_fastq)
+        (self.stdout, self.stderr) = run_command(kat_sect_cmd)
+        return self.check_files(out_file_sect)
 
-    def kat_filter(self):
-        input_fastq = self.input_seq.out_files
-        ref_fasta = self.ref_seq
-        hash_build_command = "kat filter kmer -o kat_generated_hash.kmer" + ref_fasta
+    def kat_filter_hash(self):
+        ref_fasta = self.ref_path
+        out_file_hash = os.path.join(self.out_path, "kat_generated_hash.kmer")
+        hash_build_command = "kat filter kmer -o {} {}".format(out_file_hash, ref_fasta)
         run_command(hash_build_command)
-        filter_from_hash_cmd = "kat filter seq -o kat_filtered.fastq --seq " + input_fastq + " kat_generated_hash.kmer"
-        run_command(filter_from_hash_cmd)
-        self.check_for_errors("kat_filtered.fastq")
+
+        input_fastq = self.input_path.out_files
+        out_file_filter = os.path.join(self.out_path, "kat_filtered.fastq")
+        filter_from_hash_cmd = "kat filter seq -o {} --seq {} {}".format(out_file_filter, input_fastq, out_file_hash)
+        (self.stdout, self.stderr) = run_command(filter_from_hash_cmd)
+        return self.check_files(out_file_filter)
 
     def kat_hist(self):
         input_fastq = self.input_seq.out_files
-        kat_hist_cmd = "kat hist -o hist_file.hist" + input_fastq
-        run_command (kat_hist_cmd)
-        self.check_for_errors("hist_file.hist")
+        out_file_hist = os.path.join(self.out_path, "hist_file.hist")
+        kat_hist_cmd = "kat hist -o {} {}".format(out_file_hist, input_fastq)
+        (self.stdout, self.stderr) = run_command (kat_hist_cmd)
+        return self.check_files(out_file_hist)
 
-    def check_for_errors(self, output_file):
-        if run_command.stdout != 0:
-            print("Error: ", run_command.stderr)
-            exit()
-        elif os.path.getsize (output_file):
-            print("Error: ", output_file, "is an empty file")
-            exit()
+    def check_files(self, output_file):
+       return os.path.isfile(output_file) and os.path.getsize(output_file) > 0
+
+
+sample1 = Sequence("Illumina", ["reads1.fasta", "reads2.fasta"])
+kat_run = kat_analysis(sample1, ref)
+status = kat_run.kat_sect()
 
 
